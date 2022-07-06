664f7a3cb188e1a9623cfd5f4a160989f7f59c66
Qiurong <3329279429@qq.com>
2022-07-06 21:00:21 +0800

    feat(api.js): 封装api

---
+++ src/api/user.js
@@ -0,0 +1,37 @@
+import request from '@/utils/request'
+
+// 验证码接口
+const getCaptcha = () => {
+  return request({ url: '/captcha', method: 'GET' })
+}
+
+// 登录接口
+const login = (data) => {
+  return request({
+    url: `/login?username=${data.username}&password=${data.password}&code=${data.code}&token=${data.token}`,
+    method: 'POST'
+  })
+}
+
+// 获取用户信息
+const getUserInfo = () => {
+  return request({ url: '/sys/userInfo', method: 'GET' })
+}
+
+// 权限数据
+const getNav = () => {
+  return request({ url: '/sys/menu/nav', method: 'GET' })
+}
+
+// 退出登录接口
+const logout = () => {
+  return request({ url: 'logout', method: 'POST' })
+}
+
+export default {
+  getCaptcha,
+  login,
+  getUserInfo,
+  getNav,
+  logout
+}
